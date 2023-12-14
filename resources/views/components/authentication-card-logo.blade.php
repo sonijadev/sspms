5a7431750201900ffd08c065b2e031c1baebd0e6
radoradeon <radoslawhirek@gmail.com>
2023-12-14 22:58:10 +0100

    login page change logo

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,1 +1,4 @@
-<h2 style="font-weight: bold; text-align: center; padding-bottom: 15px;">Sonija Select.</h2>
+<h2 style="font-weight: bold; text-align: center; padding-bottom: 15px;"><span style="color:;"></span>Sonija Select<span style="font-weight: bold; font-size: 3rem; color: #0d6efd;">.</span></h2>
+
+<!-- <span style="color:#6600FF;">S</span><span style="color:#000033;">o</span><span style="color:#FF3333;">n</span><span style="color:#5F9F9F;">i</span><span style="color:#93DB70;">j</span><span style="color:#EBC79E;">a</span>
+<span style="color:;">S</span><span style="color:;">e</span><span style="color:;">l</span><span style="color:;">e</span><span style="color:;">c</span><span style="color:;">t</span><span style="font-weight: bold;">.</span> -->
